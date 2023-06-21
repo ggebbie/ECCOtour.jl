92fe92b1fe385c57ce898fc17b3e5e50c31c2ba2
G Jake Gebbie <ggebbie@whoi.edu>
2023-06-20 22:44:10 -0400

    comment out PyCall build

--- deps/build.jl
+++ deps/build.jl
@@ -8,11 +8,11 @@ println("building ECCOtour package")
 
 if lowercase(get(ENV, "CI", "false")) == "true"    
 
-    ENV["PYTHON"] = ""
-    Pkg.build("PyCall")
+    #ENV["PYTHON"] = ""
+    #Pkg.build("PyCall")
 
-    Conda.add("matplotlib",channel="conda-forge")
-    Conda.add("shapely",channel="conda-forge")
-    Conda.add("cartopy=0.20.0",channel="conda-forge")
+    #Conda.add("matplotlib",channel="conda-forge")
+    #Conda.add("shapely",channel="conda-forge")
+    #Conda.add("cartopy=0.20.0",channel="conda-forge")
 
 end
