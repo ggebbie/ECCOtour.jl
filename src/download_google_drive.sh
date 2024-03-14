e7118f41d9588db44f5d2a1d67657337f9086497
G Jake Gebbie <ggebbie@whoi.edu>
2024-03-14 16:32:35 -0400

    move transport input file to whoi web

--- src/download_google_drive.sh
+++ src/download_google_drive.sh
@@ -4,4 +4,5 @@
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Sst5Y9AUbef1-Vk2ocBgOOiI2kudYRPx' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Sst5Y9AUbef1-Vk2ocBgOOiI2kudYRPx" -O state_3d_set1.0000000732.tar.gz && rm -rf /tmp/cookies.txt
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KKk8d_1nQFbM9xQjTelCmWTMfK3SA7U5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KKk8d_1nQFbM9xQjTelCmWTMfK3SA7U5" -O trsp_3d_set1.0000000732.tar.gz && rm -rf /tmp/cookies.txt
+# Google Drive now failing
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KKk8d_1nQFbM9xQjTelCmWTMfK3SA7U5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KKk8d_1nQFbM9xQjTelCmWTMfK3SA7U5" -O trsp_3d_set1.0000000732.tar.gz && rm -rf /tmp/cookies.txt
